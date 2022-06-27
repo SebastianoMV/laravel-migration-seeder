3762a2af05f5d11b4974446fc69d8e756c67ce0e
SebastianoMV <sebastianovisco@gmail.com>
2022-06-27 17:08:53 +0200

    first commit

---
+++ database/migrations/2022_06_27_145011_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure', 30);
+            $table->string('arrival', 30);
+            $table->dateTime('departure_time', 0);
+            $table->string('code', 10);
+            $table->tinyInteger('coach', 10);
+            $table->boolean('on_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
